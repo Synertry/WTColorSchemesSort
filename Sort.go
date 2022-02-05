1990be249878a12113d01018df3782e481027052
Synertry <jonny.somrak@gmail.com>
2022-02-05 23:39:45 +0100

    Copyright header update

--- Sort.go
+++ Sort.go
@@ -1,5 +1,5 @@
 /*
- *           Copyright Joe Coder 2004 - 2006.
+ *           Copyright Synertry 2022.
  *  Distributed under the Boost Software License, Version 1.0.
  *     (See accompanying file LICENSE_1_0.txt or copy at
  *           https://www.boost.org/LICENSE_1_0.txt)
